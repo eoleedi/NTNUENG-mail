edac55cdc4aaca4ccb6b37f41ac67ae586ab2ffa
eoleedi <eoleedimin@gmail.com>
2020-10-06 16:53:47 +0800

    [UPDATE] navigate bar

---
+++ www/template/nav.php
@@ -0,0 +1,13 @@
+<!-- nav bar -->
+<nav class="navbar navbar-light " style="background-color:#44090d">
+    <a class="navbar-brand" href="#">
+        <img src="./src/ntnu_logo.png"height="30" class="d-inline-block align-top" alt="">
+        <font color="white">NTNUENG Mail</font>
+    </a>
+    
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item active">
+            <a class="nav-link" href="./add.php"><font color="white">新增</font></a>
+        </li>
+    </ul>
+</nav>
