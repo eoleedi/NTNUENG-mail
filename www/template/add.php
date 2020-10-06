a3b2ef16d596a1519d7d1cf66b567c0c8de235eb
eoleedi <eoleedimin@gmail.com>
2020-10-06 16:53:19 +0800

    [UPDATE] 版面

--- www/template/add.php
+++ www/template/add.php
@@ -31,53 +31,68 @@
 		}
 		?>
 	</div>
+	<?php include("nav.php"); ?> 
+
+	
 	<form method="post">
-		<table class="table">
-			<thead>
-				<tr>
-					<th>日期: </th>
-					<th>送貨單位/師大收發室:</th>
-					<th>收件人:  </th>
-					<th>簽收日期/時間: </th>
-
-					<th>簽收人: </th>
-					<th>郵件類別: </th>
-					<th>放置日期/時間: </th>
-					<th>放置地點: </th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td><input type="date" name="cdate" value="<?php echo htmlspecialchars($cdate); ?>"></td>
-					<td><input type="text" name="senderUnit" width="50px" value="<?php echo htmlspecialchars($senderUnit); ?>"></td>
-					<td><input type="text" name="receiver" value="<?php echo htmlspecialchars($receiver); ?>"></td>
-					<td>
-						<input type="date" name="receiveDate" value="<?php echo htmlspecialchars($receiveDate); ?>">
-						<input type="time" name="receiveTime" value="<?php echo htmlspecialchars($receiveTime); ?>">
-					</td>
-
-					<td><input type="text" name="signer"value="<?php echo htmlspecialchars($signer); ?>"></td>
-					<td>
-						<select name="mailType" id="mailType" onchange="changeMailNumberName();">
-							<option selected>平信</option>
-							<option>公文</option>
-							<option>包裹</option>
-							<option>掛號信</option>
-						</select>
-						<br>
-						<label id="mailNumberName"></label>
-						<input type="text" name="mailNumber" id="mailNumber" style="display: none;" value="<?php echo htmlspecialchars($mailNumber); ?>">
-					</td>
-					<td>
-						<input type="date" name="placementDate" value="<?php echo htmlspecialchars($placementDate); ?>">
-						<input type="time" name="placementTime" value="<?php echo htmlspecialchars($placementTime); ?>">
-					</td>
-					<td><input type="text" name="placementLocation" value="<?php echo htmlspecialchars($placementLocation); ?>"></td>
-				</tr>
-			</tbody>
-		</table>
-		<div class="d-flex justify-content-center">
-			<button class="btn btn-primary" name="post" value="submit" type="submit" >增加簽收物品</button>
+		<div class="mx-auto " style="width: 80%; padding: 20px;">
+			<h3>新增簽收物品</h3>
+			<div style="border-width:2px; border-style:solid; border-color:#EEEEEE; box-shadow: 3px 3px  rgba(0, 0, 0, 0.3);">
+				<table class="table">
+					<tbody>
+						<tr>
+							<th>日期: </th>
+							<td><input type="date" name="cdate" value="<?php echo htmlspecialchars($cdate); ?>"></td>
+						</tr>
+						<tr>
+							<th>送貨單位/師大收發室:</th>
+							<td><input type="text" name="senderUnit" width="50px" value="<?php echo htmlspecialchars($senderUnit); ?>"></td>
+						</tr>
+						<tr>
+							<th>收件人:  </th>
+							<td><input type="text" name="receiver" value="<?php echo htmlspecialchars($receiver); ?>"></td>
+						</tr>	
+						<tr>
+							<th>簽收日期/時間: </th>
+							<td>
+								<input type="date" name="receiveDate" value="<?php echo htmlspecialchars($receiveDate); ?>">
+								<input type="time" name="receiveTime" value="<?php echo htmlspecialchars($receiveTime); ?>">
+							</td>
+						</tr>
+						<tr>
+							<th>簽收人: </th>
+							<td><input type="text" name="signer"value="<?php echo htmlspecialchars($signer); ?>"></td>
+						</tr>
+						<tr>
+							<th>郵件類別: </th>
+							<td>
+								<select name="mailType" id="mailType" onchange="changeMailNumberName();">
+									<option selected>平信</option>
+									<option>公文</option>
+									<option>包裹</option>
+									<option>掛號信</option>
+								</select>
+								<br>
+								<label id="mailNumberName"></label>
+								<input type="text" name="mailNumber" id="mailNumber" style="display: none;" value="<?php echo htmlspecialchars($mailNumber); ?>">
+							</td>
+						</tr>
+						<tr>
+							<th>放置日期/時間: </th>
+							<td>
+								<input type="date" name="placementDate" value="<?php echo htmlspecialchars($placementDate); ?>">
+								<input type="time" name="placementTime" value="<?php echo htmlspecialchars($placementTime); ?>">
+							</td>
+						<tr>
+							<th>放置地點: </th>
+							<td><input type="text" name="placementLocation" value="<?php echo htmlspecialchars($placementLocation); ?>"></td>
+						</tr>
+					</tbody>
+				</table>
+				<div class="d-flex justify-content-center">
+					<button class="btn btn-primary" name="post" value="submit" type="submit" >增加簽收物品</button>
+				</div>
+			</div>
 		</div>
 	</form>
 
--- www/template/index.php
+++ www/template/index.php
@@ -2,64 +2,66 @@
 <html lang="zh-TW">
 	<head>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">	
+		<title>NTNUENG Mail</title>
+		<link rel="stylesheet" href="./css/index.css">
 	</head>
-<body>
-	<nav class="navbar navbar-light bg-light">
-		<a class="navbar-brand" href="#">
-			<!-- <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
-			NTNUENG Mail -->
-		</a>
-	</nav>
+<body>	
+	
+	<?php include("nav.php"); ?>
+	
 	<br><br><br>
 	<!-- search box -->
 	<div class="mx-auto" style="width: 350px;">
 		<form class="form-inline">
-			<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+			<input class="form-control mr-sm-2" name="q" type="search" placeholder="Search" aria-label="Search">
 			<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
 		</form>
 	</div>
 	<br>
 	<!-- data -->
-	<div class="mx-auto" style="width: 80%">
-		<table class="table">
-			<thead class="thead-dark">
-				<tr>
-					<th scope="col">日期: </th>
-					<th scope="col">送貨單位/師大收發室:</th>
-					<th scope="col">收件人:  </th>
-					<th scope="col">簽收日期/時間: </th>
-
-					<th scope="col">簽收人: </th>
-					<th scope="col">郵件類別: </th>
-					<th scope="col">放置日期/時間: </th>
-					<th scope="col">放置地點: </th>
-					<th scope="col"></th>
-					<th scope="col"></th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php foreach ($goods as $d) { ?>
+	<div class="mx-auto " style="width: 80%">
+		<div style="border-width:2px; border-style:solid; border-color:#EEEEEE; box-shadow: 3px 3px 2px rgba(0, 0, 0, 0.3);">
+			<table class="table">
+				<thead class="thead-dark">
 					<tr>
-						<td><?php echo htmlspecialchars($d['cdate']); ?></td>
-						<td><?php echo htmlspecialchars($d['senderUnit']); ?></td>
-						<td><?php echo htmlspecialchars($d['receiver']); ?></td>
-						<td><?php echo htmlspecialchars( date("Y-m-d H:i", strtotime($d['receiveDateTime'])) ); ?></td>
+						<th scope="col">日期: </th>
+						<th scope="col">送貨單位/師大收發室:</th>
+						<th scope="col">收件人:  </th>
+						<th scope="col">簽收日期/時間: </th>
 
-						<td><?php echo htmlspecialchars($d['signer']); ?></td>
-						<td><?php echo htmlspecialchars($d['mailType']); ?></textarea><?php echo htmlspecialchars($d['mailNumber']); ?></td>
-						<td><?php echo htmlspecialchars( date("Y-m-d H:i", strtotime($d['placementDateTime'])) ); ?></td>
-						<td><?php echo htmlspecialchars($d['placementLocation']); ?></td>
-
-						<td><a href="mod.php?id=<?php echo htmlspecialchars($d['id']); ?>">Modify</a></td>
-						<td><a href="del.php?id=<?php echo htmlspecialchars($d['id']); ?>" onclick="return confirm('Delete This?')">Delete</a></li>
-
+						<th scope="col">簽收人: </th>
+						<th scope="col">郵件類別: </th>
+						<th scope="col">放置日期/時間: </th>
+						<th scope="col">放置地點: </th>
+						<th scope="col"></th>
+						<th scope="col"></th>
 					</tr>
-				<?php } ?>
-			</tbody>
-		</table>
+				</thead>
+				<tbody>
+					<?php foreach ($goods as $d) { ?>
+						<tr>
+							<td><?php echo htmlspecialchars($d['cdate']); ?></td>
+							<td><?php echo htmlspecialchars($d['senderUnit']); ?></td>
+							<td><?php echo htmlspecialchars($d['receiver']); ?></td>
+							<td><?php echo htmlspecialchars( date("Y-m-d H:i", strtotime($d['receiveDateTime'])) ); ?></td>
+
+							<td><?php echo htmlspecialchars($d['signer']); ?></td>
+							<td><?php echo htmlspecialchars($d['mailType']); ?></textarea><?php echo htmlspecialchars($d['mailNumber']); ?></td>
+							<td><?php echo htmlspecialchars( date("Y-m-d H:i", strtotime($d['placementDateTime'])) ); ?></td>
+							<td><?php echo htmlspecialchars($d['placementLocation']); ?></td>
+
+							<td><a href="mod.php?id=<?php echo htmlspecialchars($d['id']); ?>">Modify</a></td>
+							<td><a href="del.php?id=<?php echo htmlspecialchars($d['id']); ?>" onclick="return confirm('Delete This?')">Delete</a></li>
+
+						</tr>
+					<?php } ?>
+				</tbody>
+			</table>
+			<div class="d-flex justify-content-center">
+				<button class="btn btn-primary" onclick="location.href='add.php'">Add News</button>
+			</div>
+		</div>
 	</div>
-	<div class="d-flex justify-content-center">
-		<button class="btn btn-primary" onclick="location.href='add.php'">Add News</button>
-	</div>
+	
 </body>
 </html>
