5b0eb62d161a52877c3105700059ad97fec7b5c5
eoleedi <eoleedimin@gmail.com>
2020-10-08 04:09:55 +0800

    [UPDATE] clockpicker

---
+++ www/clock.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">	
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-clockpicker.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-components.css" media="screen">
+
+	<title>NTNUENG Mail</title>
+	
+</head>
+<body>	
+	
+	<div class="input-group clockpicker">
+		<input type="text" class="form-control" value="09:30">
+		<div class="input-group-append">
+			<span class="input-group-text">
+				<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-clock" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+					<path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm8-7A8 8 0 1 1 0 8a8 8 0 0 1 16 0z"/>
+					<path fill-rule="evenodd" d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5z"/>
+				</svg>
+    		</span>
+  		</div>
+	</div>
+	
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="js/bootstrap-clockpicker.min.js"></script>
+
+	
+
+	<script type="text/javascript">
+	$('.clockpicker').clockpicker();
+	</script>
+</body>
+</html>
