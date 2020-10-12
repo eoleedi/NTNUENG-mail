1e1e3204bd80db835aed57718baa3afdc5968385
eoleedi <eoleedimin@gmail.com>
2020-10-12 17:42:35 +0800

    [UPDATE] update nav bar

--- www/template/nav.php
+++ www/template/nav.php
@@ -1,13 +1,17 @@
 <!-- nav bar -->
 <nav class="navbar navbar-light " style="background-color:#44090d">
     <a class="navbar-brand" href="./">
-        <img src="./src/ntnu_logo.png"height="30" class="d-inline-block align-top" alt="">
-        <font color="white">NTNUENG Mail</font>
+        <img src="./src/ntnu_logo.png" height="50" class="d-inline-block align-middle" alt="">
+        <font color="white" >英語系郵務登記平台</font>
     </a>
     
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
         <li class="nav-item active">
-            <a class="nav-link" href="./add.php"><font color="white">新增</font></a>
+            <a class="nav-link" href="./add.php">
+            <button class="btn btn-dark">
+                <font color="white">新增</font>
+            </button>
+            </a>
         </li>
     </ul>
 </nav>
