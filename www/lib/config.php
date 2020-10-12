14bb9058f0e588fe7a50da7a21a8ae1ae4e5cf22
eoleedi <eoleedimin@gmail.com>
2020-10-12 17:42:15 +0800

    [DEBUG] root user

--- www/lib/config.php
+++ www/lib/config.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
+define('DB_USER', 'root123');
+define('DB_PASS', 'root123');
 define('DB_NAME', 'storage');
 date_default_timezone_set('Asia/Shanghai');
 ?>
