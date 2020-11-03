a891fd69e453013c9a48ee73b45d9e78506fd1fe
eoleedi <eoleedimin@gmail.com>
2020-11-03 12:02:59 +0800

    [UPDATE] .gitignore

--- html/lib/config.php
+++ html/lib/config.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_USER', 'ntnuengdbadmin');
-define('DB_PASS', 'n6484tnueng');
+define('DB_USER', 'root123');
+define('DB_PASS', 'root123');
 define('DB_NAME', 'storage');
 date_default_timezone_set('Asia/Shanghai');
 ?>
