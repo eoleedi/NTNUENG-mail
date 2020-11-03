d6b806a217db3f83cdd6ff0b33d4b0ca44c0dc2c
eoleedi <eoleedimin@gmail.com>
2020-11-03 11:59:20 +0800

    [UPDATE]  clear up

--- certbot.sh
+++
@@ -1,1 +0,0 @@
-docker run --rm -v /etc/letsencrypt:/etc/letsencrypt -v ~/NTNUENG_mail/html:/html -ti certbot/certbot certonly --email eoleedimin@gmail.com --agree-tos --webroot -w /html -d cas.eng.ntnu.edu.tw
---
+++ certbot/certbot.sh
@@ -0,0 +1,2 @@
+# ONLY FOR ENG 
+docker run --rm -v ~/NTNUENG_mail/certbot/letsencrypt:/etc/letsencrypt -v ~/NTNUENG_mail/html:/html -ti certbot/certbot certonly --email eoleedimin@gmail.com --agree-tos --webroot -w /html -d cas.eng.ntnu.edu.tw
