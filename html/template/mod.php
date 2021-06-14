8807dc2aec4b9d946210fc9a83d7fbff1815c20d
eoleedi_eng <eoleedi@ntnueng.co.cc>
2021-06-14 10:46:43 +0000

    [BUGFIX] force update of css file

--- html/template/mod.php
+++ html/template/mod.php
@@ -6,7 +6,7 @@
 	<meta name="viewport", content="width = device-width, initial-scale = 1.0">
 	<link rel="icon" href="/src/favicon.png">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-	<link rel="stylesheet" href="./css/modadd.css">
+	<link rel="stylesheet" href="./css/modadd.css?v=2">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap-clockpicker.min.css">
 	
 	<script>
